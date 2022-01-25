0c6e16fce82273b3d0ba4635b28d5d3b0126c1a7
chelsea helion <42006195+chelsea-helion@users.noreply.github.com>
2022-01-25 18:21:18 -0500

    Update SC_homework.R

--- SC_homework.R
+++ SC_homework.R
@@ -17,4 +17,30 @@ newDat <- SCdat %>%
   select(justiceName, vote, direction) %>% 
   filter(vote == 1 | vote == 2)
 
-newDat$justiceName %in% Pdat$justiceName
+## check that the names match
+all(newDat$justiceName %in% Pdat$justiceName)
+
+## move party into the new dataframe
+newDat <- inner_join(newDat, Pdat)
+
+## combine across votes based on justice
+
+
+
+## fit bayesian model
+fit1 <- 
+  brm(data = newDat,
+      family = binomial(link = logit),
+      majVotes | trials(vote) ~ 1 + (1|Party) + (1|Party:justiceName),
+      prior = c(prior(normal(0,1.5), class = Intercept),
+                prior(normal(0,1), class = sd)),
+      iter = 3500, warmup = 500, chains = 3, cores = 3,
+      control = list(adapt_delta = .99), 
+      seed = 9)
+
+color_scheme_set("blue")
+plot(fit2)
+
+
+
+
